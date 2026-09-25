feb589c2cb7b757f49b698588ad7fd12eed4a6bc
mansipatil12345 <patilmansi7722@gmail.com>
2026-09-25 21:42:19 +0530

    Revised concepts

--- Practice/Practice4.java
+++ Practice/Practice4.java
@@ -216,3 +216,20 @@
 //         pairs(arr);
 //     }
 // }
+
+
+public class Practice4{
+    public static void pairs(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int curr = arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("("+curr+","+j+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        pairs(arr); 
+    }
+}
